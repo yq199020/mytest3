4e26de25a280eae3ee54cfe64064a4edcc8fa76f
admin <yiqi2012@126.com>
2019-07-26 18:26:05 +0800

    firstCommit

---
+++ gitTest/src/main/java/pojo/Student.java
@@ -0,0 +1,11 @@
+package pojo;
+
+/**
+ * @Author: yiqi
+ * @Description:
+ * @Date: Create in 18:25 2019/7/26
+ */
+public class Student {
+
+
+}
--- gitTest/src/main/java/pojo/User.java
+++ gitTest/src/main/java/pojo/User.java
@@ -10,4 +10,5 @@ public class User {
     private String name;
     private int age;
 
+    private int i = 100;
 }
